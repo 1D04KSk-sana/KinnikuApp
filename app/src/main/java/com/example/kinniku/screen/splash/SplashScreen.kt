a55aca6350e8fbf1ccd31fffc2b12d1554092f94
1D04KSk-sana <C2105@oic.jp>
2023-08-29 01:06:34 +0900

    splashscreen make

--- app/src/main/java/com/example/kinniku/screen/splash/SplashScreen.kt
+++ app/src/main/java/com/example/kinniku/screen/splash/SplashScreen.kt
@@ -1,10 +1,19 @@
 package com.example.kinniku.screen.splash
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.kinniku.R
+import com.example.kinniku.screen.component.CenterProgressIndicator
 import com.example.kinniku.ui.theme.SubRed
 
 //画面番号：
@@ -27,6 +36,30 @@ private fun Logo(
             .fillMaxWidth(),
         color = SubRed
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.logo_wht),
+            contentDescription = "ロゴ画像",
+            modifier = modifier
+                .defaultMinSize(72.dp)
+                .padding(bottom = 20.dp),
+            contentScale = ContentScale.Inside,
+        )
 
+        if (isVisibleProgress) {
+            CenterProgressIndicator(
+                modifier = modifier
+            )
+        }
     }
+}
+
+@Preview
+@Composable
+fun PreviewLogo(
+    modifier: Modifier = Modifier
+) {
+    Logo(
+        modifier = modifier,
+        isVisibleProgress = true
+    )
 }
