ac6079d9ddd2ca884d1d92013c8746757074357f
1D04KSk-sana <C2105@oic.jp>
2023-08-29 02:40:55 +0900

    loginscreen make

--- app/src/main/java/com/example/kinniku/screen/login/LoginScreen.kt
+++ app/src/main/java/com/example/kinniku/screen/login/LoginScreen.kt
@@ -1,2 +1,79 @@
 package com.example.kinniku.screen.login
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.kinniku.R
+import com.example.kinniku.ui.theme.MainRed
+import com.example.kinniku.ui.theme.MiniStyle
+
+@Composable
+fun LoginScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
+
+@Composable
+fun LoginScreenContent(
+    modifier: Modifier = Modifier,
+    isError: Boolean = false
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Spacer(
+                modifier = Modifier.height(32.dp)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.logo_wht),
+                contentDescription = "ロゴ画像"
+            )
+
+            if (isError) {
+                Column {
+                    Spacer(
+                        modifier = Modifier
+                            .height(32.dp)
+                    )
+                    Text(
+                        text = stringResource(id = R.string.login_screen_error_message),
+                        color = MainRed,
+                        style = MiniStyle
+                    )
+                }
+            }
+
+            Spacer(
+                modifier = Modifier
+                    .padding(top = 30.dp)
+            )
+            
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewLoginScreenContent() {
+    LoginScreenContent()
+}
--- app/src/main/java/com/example/kinniku/ui/theme/Type.kt
+++ app/src/main/java/com/example/kinniku/ui/theme/Type.kt
@@ -15,20 +15,11 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+)
+
+val MiniStyle = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.Normal,
+    fontSize = 12.sp,
+    lineHeight = 18.sp
 )
