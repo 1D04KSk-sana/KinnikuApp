066b65092c1032ff1a8a5458ad44716710fa2080
1D04KSk-sana <C2105@oic.jp>
2023-08-29 12:36:53 +0900

    folda make

--- app/src/main/java/com/example/kinniku/screen/calendar/CalendarScreen.kt
+++ app/src/main/java/com/example/kinniku/screen/calendar/CalendarScreen.kt
@@ -1,2 +1,20 @@
 package com.example.kinniku.screen.calendar
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun CalendarScreen() {
+
+}
+
+@Composable
+fun CalendarScreenContent() {
+
+}
+
+@Preview
+@Composable
+fun PreviewCalendarScreenContent() {
+
+}
